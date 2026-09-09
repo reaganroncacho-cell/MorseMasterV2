211f00d6c423af0f8d2dd7f72edaa1b7e7d875af
reaganroncacho-cell <reaganroncacho@gmail.com>
2026-09-09 17:45:09 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/example/morsemaster/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.morsemaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
+    }
+}
